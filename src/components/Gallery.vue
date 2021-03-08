edd9961d8ec97f15759a453b6e1c430b1987191c
Tatiana Kh <ts.khol@gmail.com>
2021-03-08 19:45:44 +0300

    implement initial UI

---
+++ src/components/Gallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="gallery">
+    <img class="gallery__image" src="../assets/image-2.jpg" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.gallery {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.gallery__image {
+  height: 100%;
+  max-width: 100%;
+  object-fit: cover;
+}
+</style>
